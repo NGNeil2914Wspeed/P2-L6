76ce4bac7341475141ed00d638ac82dbb9df9b17
NGNeil2914Wspeed <neilgupta777@gmail.com>
2026-01-31 11:33:46 +0530

    Add files via upload

---
+++ Floyds triangle.py
@@ -0,0 +1,8 @@
+from time import sleep
+num = 1
+
+for i in range(1, 11):
+    for j in range(1, i+1):
+        print (f"{num}", end="  ")
+        num+=1
+    print ()
---
+++ Pattern slide.py
@@ -0,0 +1,9 @@
+from time import sleep
+i = 1
+txt="*"
+
+for i in range(10):
+    print (txt)
+    txt = txt+" *"
+    sleep(0.1)
+
